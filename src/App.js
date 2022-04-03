0729d1342ac5651fa2fd83f016f6767579d02803
Chris Chicas <christianchicas7@gmail.com>
2022-04-03 11:07:31 -0400

    redux implemented

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import './App.css';
 import Nav from './components/Nav'
 import ContentWrapper from './components/ContentWrapper'
 import Footer from './components/Footer'
+import {useSelector, useDispatch} from "react-redux"
+import {lightMode, darkMode} from "./features/modeSlice"
 
 function App() {
+  const mode = useSelector(state => state.mode)
+  const dispatch = useDispatch()
+
+  const modeSwap = () => {
+    mode.darkMode ? dispatch(lightMode()) : dispatch(darkMode())
+  }
   
   return (
-    <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
+    <div style={{ backgroundColor: mode.color1, color: 'black' }} className="App">
       <Nav />
+      <button onClick={modeSwap}>{mode.darkMode ? "Light Mode" : "Dark Mode"}</button>
       <ContentWrapper />
       <Footer />
     </div>
